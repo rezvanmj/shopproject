8a888389a1d3923fe0ecb77cbfa94c6ab112c684
rezvanmj <rezvan.mjj@gmail.com>
2021-03-07 05:20:54 -0800

    new changes

--- petShop/Petshop/src/app/app.module.ts
+++ petShop/Petshop/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {Router,RouterModule,Routes} from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { ShoppingBasketComponent } from './shopping-basket/shopping-basket.component';
+import { ProductOffersComponent } from './product-offers/product-offers.component';
+
+
+const routes : Routes =[
+  {path:"" , component: HomeComponent},
+  {path: "product" , component:ProductComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    ShoppingBasketComponent,
+    ProductOffersComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
